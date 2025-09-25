3486b4bdddd1e18772e9eb4ff206af67f992e8b4
theubusu <80545678+theubusu@users.noreply.github.com>
2025-09-25 23:06:06 +0200

    Major cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::{Path};
 use std::fs::{File, OpenOptions};
-use std::io:: {Write, Read, Seek, SeekFrom, Cursor};
+use std::io:: {self, Write, Read, Seek, SeekFrom, Cursor};
 
 use clap::Parser;
 use aes::Aes128;
@@ -29,11 +29,34 @@ struct Section {
     name: String,
 }
 
-fn decrypt_aes(encrypted_data: &[u8], key: &[u8; 16], iv: &[u8; 16]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn read_exact<R: Read>(reader: &mut R, size: usize) -> io::Result<Vec<u8>> {
+    let mut buf = vec![0u8; size];
+    reader.read_exact(&mut buf)?;
+    Ok(buf)
+}
+
+fn decrypt_aes_salted(encrypted_data: &[u8], passphrase_bytes: &Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let mut data = encrypted_data.to_vec();
-    let decryptor = Aes128CbcDec::new(key.into(), iv.into());
-    let decrypted = decryptor.decrypt_padded_mut::<Pkcs7>(&mut data)
-        .map_err(|e| format!("!!Decryption error!!: {:?}", e))?;
+
+    assert!(String::from_utf8(data[0..8].to_vec())? == "Salted__", "invalid encrypted data!");
+    let salt = &data[8..16];
+
+    //key = md5 of (passphrase + salt)
+    let mut key = Vec::new();
+    key.extend_from_slice(&passphrase_bytes);
+    key.extend_from_slice(&salt);
+    let key_md5 = md5::compute(&key);
+
+    //iv = md5 of (md5 of key + passphrase + salt)
+    let mut iv = Vec::new();
+    iv.extend_from_slice(&key_md5.0);
+    iv.extend_from_slice(&passphrase_bytes);
+    iv.extend_from_slice(&salt);
+    let iv_md5 = md5::compute(&iv);
+
+    let decryptor = Aes128CbcDec::new((&key_md5.0).into(), (&iv_md5.0).into());
+    let decrypted = decryptor.decrypt_padded_mut::<Pkcs7>(&mut data[16..])
+        .map_err(|e| format!("Decryption error!!: {:?}", e))?;
     
     Ok(decrypted.to_vec())
 }
@@ -53,17 +76,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let keep_sign = args.keep_sign;
 
-    let mut magic = [0u8; 6];
-    file.read_exact(&mut magic)?; 
+    //magic
+    let magic = read_exact(&mut file, 6)?;
     if &magic != b"MSDU10" {
-        eprintln!("Error: Not a valid MSD file!");
+        eprintln!("Error: Not a valid legacy MSD file!");
         std::process::exit(1);
     }
     
-    let mut section_count_bytes = [0u8; 4];
-    file.read_exact(&mut section_count_bytes)?;
-    let section_count = u32::from_le_bytes(section_count_bytes);
-    
+    //section count
+    let section_count_bytes = read_exact(&mut file, 4)?;
+    let section_count = u32::from_le_bytes(section_count_bytes.try_into().unwrap());
     println!("Number of sections: {}", section_count);
 
     let mut sections: Vec<Section> = Vec::new();
@@ -71,56 +93,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //parse sections
     for _i in 0..section_count {
         // 4 bytes index
-        let mut index_bytes = [0u8; 4];
-        file.read_exact(&mut index_bytes)?;
-        let index = u32::from_le_bytes(index_bytes);
+        let index_bytes = read_exact(&mut file, 4)?;
+        let index = u32::from_le_bytes(index_bytes.try_into().unwrap());
         
         // 4 bytes offset
-        let mut offset_bytes = [0u8; 4];
-        file.read_exact(&mut offset_bytes)?;
-        let offset = u32::from_le_bytes(offset_bytes);
+        let offset_bytes = read_exact(&mut file, 4)?;
+        let offset = u32::from_le_bytes(offset_bytes.try_into().unwrap());
         
         // 4 bytes size
-        let mut size_bytes = [0u8; 4];
-        file.read_exact(&mut size_bytes)?;
-        let size = u32::from_le_bytes(size_bytes);
+        let size_bytes = read_exact(&mut file, 4)?;
+        let size = u32::from_le_bytes(size_bytes.try_into().unwrap());
 
         println!("Section {}: offset: {}, size: {}", index, offset, size);
         sections.push(Section { index, offset, size , name: "".to_string() });
     }
 
     //read TOC entry
-    let mut toc_entry_bytes = [0u8; 4];
-    file.read_exact(&mut toc_entry_bytes)?;
-    let toc_entry = u32::from_le_bytes(toc_entry_bytes);
+    let toc_entry_bytes = read_exact(&mut file, 4)?;
+    let toc_entry = u32::from_le_bytes(toc_entry_bytes.try_into().unwrap());
     assert!(toc_entry == 0, "invalid TOC entry!");
 
-    // 4 bytes index
-    let mut toc_index_bytes = [0u8; 4];
-    file.read_exact(&mut toc_index_bytes)?;
-    let _toc_index = u32::from_le_bytes(toc_index_bytes);
+    // 4 bytes index?
+    let _toc_index_bytes = read_exact(&mut file, 4)?;
 
     // 4 bytes offset
-    let mut toc_offset_bytes = [0u8; 4];
-    file.read_exact(&mut toc_offset_bytes)?;
-    let toc_offset = u32::from_le_bytes(toc_offset_bytes);
+    let toc_offset_bytes = read_exact(&mut file, 4)?;
+    let toc_offset = u32::from_le_bytes(toc_offset_bytes.try_into().unwrap());
         
     // 4 bytes size
-    let mut toc_size_bytes = [0u8; 4];
-    file.read_exact(&mut toc_size_bytes)?;
-    let toc_size = u32::from_le_bytes(toc_size_bytes);
-    println!("TOC Section: offset: {}, size: {}", toc_offset, toc_size);
+    let toc_size_bytes = read_exact(&mut file, 4)?;
+    let toc_size = u32::from_le_bytes(toc_size_bytes.try_into().unwrap());
 
+    println!("TOC Section: offset: {}, size: {}", toc_offset, toc_size);
     println!();
 
     //Read firmware name
-    let mut name_size_byte = [0u8; 1];
-    file.read_exact(&mut name_size_byte)?;
-    let name_size = u8::from_le_bytes(name_size_byte);
+    let name_size_byte = read_exact(&mut file, 1)?;
+    let name_size = u8::from_le_bytes(name_size_byte.try_into().unwrap());
 
-    let mut name_bytes = vec![0u8; name_size as usize];
-    file.read_exact(&mut name_bytes)?;
+    let name_bytes = read_exact(&mut file, name_size as usize)?;
     let name = String::from_utf8(name_bytes)?;
+
     println!("Firmware: {}", name);
 
     let mut passphrase: Option<&str> = None;
@@ -146,63 +159,45 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     //read and decrypt TOC
     file.seek(SeekFrom::Start(toc_offset as u64))?;
-    let mut encrypted_toc = vec![0u8; toc_size as usize];
-    file.read_exact(&mut encrypted_toc)?;
-
-    assert!(String::from_utf8((&encrypted_toc[0..8]).to_vec())? == "Salted__", "invalid TOC!");
-    let salt = &encrypted_toc[8..16];
-
-    let key: Vec<u8> = [&passphrase_bytes, salt].concat();
-    let key_md5 = md5::compute(&key);
-
-    let mut iv = Vec::new();
-    iv.extend_from_slice(&key_md5.0);
-    iv.extend_from_slice(&passphrase_bytes);
-    iv.extend_from_slice(&salt);
-    let iv_md5 = md5::compute(&iv);
-
-    let toc = decrypt_aes(&encrypted_toc[16..], &key_md5, &iv_md5)?;
+    let encrypted_toc = read_exact(&mut file, toc_size as usize)?;
+    let toc = decrypt_aes_salted(&encrypted_toc, &passphrase_bytes)?;
 
     let mut toc_reader = Cursor::new(toc);
 
     toc_reader.seek(SeekFrom::Current(128))?; // probably signature
-    let mut n = 1;
+    let mut n = 0;
 
     while (toc_reader.position() as usize) < toc_reader.get_ref().len() {
-        if n != 1 { //isnt on first segment
-            toc_reader.seek(SeekFrom::Current(4))?; //some magic?
+        if n != 0 { //isnt on first segment
+            toc_reader.seek(SeekFrom::Current(4))?; //some magic? seems to be 00 00 03 E8 always
         }
 
-        let mut segment_length_bytes = [0u8; 4];
-        toc_reader.read_exact(&mut segment_length_bytes)?;
-        let segment_length = u32::from_be_bytes(segment_length_bytes);
+        let segment_length_bytes = read_exact(&mut toc_reader, 4)?;
+        let segment_length = u32::from_be_bytes(segment_length_bytes.try_into().unwrap());
 
-        let mut segment_size_bytes = [0u8; 4];
-        toc_reader.read_exact(&mut segment_size_bytes)?;
-        let segment_size = u32::from_be_bytes(segment_size_bytes);
+        let segment_size_bytes = read_exact(&mut toc_reader, 4)?;
+        let segment_size = u32::from_be_bytes(segment_size_bytes.try_into().unwrap());
 
         if segment_size != 0 {
-            assert!(segment_size == sections[n - 1].size, "size in TOC does not match size from header!");
+            assert!(segment_size == sections[n].size, "size in TOC does not match size from header!");
 
             toc_reader.seek(SeekFrom::Current(26))?; //unknown now
 
-            let mut name_length_byte = [0u8; 1];
-            toc_reader.read_exact(&mut name_length_byte)?;
-            let name_length = u8::from_be_bytes(name_length_byte);
+            let name_length_byte = read_exact(&mut toc_reader, 1)?;
+            let name_length = u8::from_be_bytes(name_length_byte.try_into().unwrap());
 
-            let mut name_bytes = vec![0u8; name_length as usize];
-            toc_reader.read_exact(&mut name_bytes)?;
+            let name_bytes = read_exact(&mut toc_reader, name_length as usize)?;
             let name = String::from_utf8(name_bytes)?;
 
             //println!("Segment: name='{}', length={}, size={}", name, segment_length, segment_size);
             // apply respective names to sections
-            if n != 1 && sections[n-2].name == name{ //second section with the same name is some sort of signature
-                sections[n-1].name = name + "_sign";
+            if n != 0 && sections[n-1].name == name{ //second section with the same name is some sort of signature
+                sections[n].name = name + "_sign";
             } else {
-                sections[n-1].name = name;
+                sections[n].name = name;
             }
 
-            toc_reader.seek(SeekFrom::Current((segment_length - 31 - name_length as u32).into()))?;
+            toc_reader.seek(SeekFrom::Current((segment_length - name_length as u32 - 31).into()))?;
 
             n = n + 1;
         } else {
@@ -218,34 +213,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut out_data: Vec<u8>;
 
         if section.name.ends_with("_sign") {
-            //unknown format
+            //in raw format
             if keep_sign {
                 file.seek(SeekFrom::Start(section.offset as u64))?;
                 out_data = vec![0u8; section.size as usize];
                 file.read_exact(&mut out_data)?;
             } else {
-                println!("- Skipping signature file");
+                println!("- Skipping signature file...");
                 continue;
             }      
         } else { 
             file.seek(SeekFrom::Start(section.offset as u64 + 136))?; // skip signature
             let mut encrypted_data = vec![0u8; section.size as usize - 136];
             file.read_exact(&mut encrypted_data)?;
-
-            //decrypt
-            assert!(String::from_utf8((&encrypted_data[0..8]).to_vec())? == "Salted__", "invalid file data!");
-            let salt = &encrypted_data[8..16];
-
-            let key: Vec<u8> = [&passphrase_bytes, salt].concat();
-            let key_md5 = md5::compute(&key);
-
-            let mut iv = Vec::new();
-            iv.extend_from_slice(&key_md5.0);
-            iv.extend_from_slice(&passphrase_bytes);
-            iv.extend_from_slice(&salt);
-            let iv_md5 = md5::compute(&iv);
-
-            out_data = decrypt_aes(&encrypted_data[16..], &key_md5, &iv_md5)?;
+            out_data = decrypt_aes_salted(&encrypted_data, &passphrase_bytes)?;
         }
 
         std::fs::create_dir_all(&output_folder)?;
@@ -257,6 +238,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .open(&output_path)?;
         out_file.write_all(&out_data)?;
 
+        println!("- Saved file!");
     }
 
     println!();
